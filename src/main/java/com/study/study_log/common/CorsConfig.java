bf11b195106d218194acefbaf4c5089898aaec7c
[CLOUD4] <dbslxl11@gmail.com>
2026-03-11 18:53:18 +0900

    refactor : cors 설정

---
+++ src/main/java/com/study/study_log/common/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.study.study_log.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3001")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true); // 클라이언트가 서버와 교환하는 요청에서 쿠키 또는 인증 관련 정보를 포함할 수 있게 허용
+    }
+}
